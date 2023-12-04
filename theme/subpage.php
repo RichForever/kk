f0c98eedf3a3e2e6462eeb8eabcd3a6fc974c92c
RichForever <mateuszmisiak1@gmail.com>
2023-12-04 12:48:49 +0100

    update files

---
+++ theme/subpage.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package WordPress
+ * @subpackage wpstarter
+ * @since wpstarter 1.0
+ * Template Name: Subpage
+ */
+
+$context = Timber::context();
+
+$timber_post     = Timber::get_post();
+$context['post'] = $timber_post;
+Timber::render('subpage.twig', $context );

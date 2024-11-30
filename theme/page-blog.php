d4e2459e66dc50ad6fa4a8ad724665fb31f9a052
RichForever <mateuszmisiak1@gmail.com>
2024-11-30 18:01:59 +0100

    add author bio to blog page template, change some other settings

--- theme/page-blog.php
+++ theme/page-blog.php
@@ -10,10 +10,22 @@
 
 $context = Timber::context();
 
-$catIds = get_field('categories', $post->ID) ? get_field('categories', $post->ID) : ['12'];
+$page_title = get_field('page_title', $post->ID) ? get_field('page_title', $post->ID) : $post->post_title;
+$context['page_title'] = $page_title;
+
+$page_subtitle = get_field('page_subtitle', $post->ID);
+$context['page_subtitle'] = $page_subtitle;
+
+$show_author_bio = get_field('show_author_bio', $post->ID);
+$author_bio = get_field('author_bio', $post->ID);
+
+$author_data = array_merge(['show_author_bio' => $show_author_bio], ['author_bio' => $author_bio]);
+$context['author_data'] = $author_data;
+
+$cat_ids = get_field('categories', $post->ID) ? get_field('categories', $post->ID) : ['12'];
 $args = [
 	'post_type' => 'post',
-	'cat' => $catIds,
+	'cat' => $cat_ids,
 	'paged' => get_query_var('paged') ? get_query_var('paged') : 1
 ];
 $context['posts']      = Timber::get_posts($args);
